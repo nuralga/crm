a3d3293f4da137ff797afedfb52ed71837b1eaf2
Nurzhigit Ongalov <nur.onalov@gmail.com>
2022-04-12 17:53:45 +0600

    added new constants

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+{
+    const modalTitle = document.getElementsByClassName('modal__title')[0].innerText;
+    const modalForm = document.getElementsByClassName('modal__form')[0];
+    const discountChbx = document.getElementById('discount');
+    const discountCountInp = document.getElementsByName('discount_count')[0];
+}
